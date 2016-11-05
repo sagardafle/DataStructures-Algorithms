1cac0ddeb0e141258ab636643c8aba1f4150f8bb
Sagar Dafle <sagar.cdafle@gmail.com>
2016-11-05 04:41:28 +0000

    Merge 2 sorted array

--- LeetCode Problems/21_MergeTwoSortedArrays.java
+++ LeetCode Problems/21_MergeTwoSortedArrays.java
@@ -10,8 +10,14 @@
  /**
   * LOGIC1 : Using Priority Queue. Make use of the same logic as to merge "K" linked list. https://github.com/sagardafle/DataStructures-Algorithms/blob/master/LeetCode%20Problems/23_MergeKSortedLists.java
   */ 
+  /**
+   * LoGIC2 : Run a loop and keep comparing elements one by one.
+   */ 
 public class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
+   
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+         /** LOGIC 1:
+        ListNode[] lists = {l1,l2};
         
         //null check
         if(lists==null||lists.length==0)
@@ -33,9 +39,9 @@ public class Solution {
             pq.offer(list);
     }    
     
-    /**
-     * remove the elements from PQ and keep on adding it to the next of the head.
-     */ 
+    
+     remove the elements from PQ and keep on adding it to the next of the head.
+      
     while(!pq.isEmpty()){
        p.next = pq.poll();
       p=p.next;
@@ -44,5 +50,31 @@ public class Solution {
             pq.offer(p.next);
     }
     return head.next;
-}
+    }
+    */
+    
+    
+    /**LOGIC2: Run a loop and keep comparing elements one by one.
+     */ 
+     ListNode dummy = new ListNode(0);
+        ListNode k = dummy;
+     while(l1!=null && l2!=null){
+             if(l1.val <= l2.val){
+                 k.next = l1;
+                 l1 = l1.next;
+             } else {
+                 k.next = l2;
+                 l2 = l2.next;
+             }
+             k = k.next;
+     }
+         
+          if (l1 == null) // add l2 to k.next
+        	k.next = l2;
+        if (l2 == null)   // add l1 to k.next
+        	k.next = l1;
+         
+     return dummy.next;
+     
+    }
 }
