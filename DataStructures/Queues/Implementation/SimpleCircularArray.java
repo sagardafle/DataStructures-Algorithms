ec1c0d7c1bea155a87c7b578ae99677ec6addca5
Sagar Dafle <sagar.cdafle@gmail.com>
2016-11-22 01:49:44 +0000

    Rotate Array Java

--- DataStructures/Queues/Implementation/SimpleCircularArray.java
+++ DataStructures/Queues/Implementation/SimpleCircularArray.java
@@ -1,3 +1,14 @@
+/**
+ * LOGIC: We add elements circularly and use 2 variables to keep track of front & rear.
+ * 1. Why we need circular array instead of normal array?
+ *  - If we use a normal array, then aafter certain set of enqueue/dequeue operations, the indices at front and rear becomes useless. 
+ *  - To avoid this, we make use of a cirular array where we treat last and first eleemnts as contiguous.
+ *  - So, if we have any free slots at the beginning, the rear pointer can easily go to its next free slot.
+ * 
+ * 2. Complexity: enqueue().dequeue(),peek() and all other operations are O(1).
+ * 
+ * 3. Limitations: We always need to specify the maximum size of the queue on a prior basis. 
+ */ 
 import java.util.*;
 public class SimpleCircularArray{
     static int CAPACITY = 20;
---
+++ LeetCode Problems/198_RotateArray.java
@@ -0,0 +1,23 @@
+
+/**
+ * LOGIC : 
+ * 1. First rotate entire array. 
+ * 2. Then rotate array from 0 to k-1. 
+ * 3. Finally rotate array from k to last element.
+ */ 
+public class Solution {
+    public void rotate(int[] nums, int k) {
+       k = k%nums.length;
+       reverseArray(nums, 0, nums.length-1);
+       reverseArray(nums, 0, k-1);
+       reverseArray(nums, k, nums.length-1);
+    }
+       public void reverseArray(int nums[], int start, int end){
+           int temp;
+           while(start < end){
+               temp = nums[start];
+               nums[start++] = nums[end];
+               nums[end--] = temp;
+           }
+       }
+}
