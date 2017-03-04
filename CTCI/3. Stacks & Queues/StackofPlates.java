4b67c70cec255406405858ee71d4a1c96c82015e
Sagar Dafle <sagar.cdafle@gmail.com>
2017-03-04 00:34:12 +0000

    popAt implementation

--- CTCI/3. Stacks & Queues/StackofPlates.java
+++ CTCI/3. Stacks & Queues/StackofPlates.java
@@ -13,11 +13,14 @@ class StackofPlates{
         }
         System.out.println("No. of stacks: "+platesstack.size());
         System.out.println("Stack of Plates: "+platesstack);
+        System.out.println("=================================================");
+        System.out.println("Pop at 2nd stack: Removed value: "+sop.popAt(1));
+        System.out.println("Stack of Plates after above pop: "+platesstack);
     }
     
     
     public void push(int val){
-        Stack laststack = getLastStack();
+        Stack laststack = getLastStack(); 
         if(laststack!=null && laststack.size() < capacity) {
             //last stack has some capacity
             laststack.push(val);
@@ -38,9 +41,36 @@ class StackofPlates{
          return val;
     }
     
+    /**
+     * Returns the last stack in the list.
+     */ 
     public Stack getLastStack(){
        if(platesstack.size()==0) return null;
        return platesstack.get(platesstack.size()-1) ;
     }
     
+    /**
+     * Pop the topmost element of a specific stack in the plates-stack.
+     */ 
+    public int popAt(int index){
+        if(index > platesstack.size()-1) {
+            System.out.println("Invalid stack index");
+            return -1;
+        }
+        Stack s = platesstack.get(index);
+        int val = (int) s.pop();
+        balance(index,index+1); //Balance the stack such as all the stacks except the last one are at their full capacity.
+        return val;
+    }
+    
+    /**
+     * We will try to balance the left stack and right stack such that the void space in the left stack is filled back by the right stack.
+     */ 
+    public void balance(int left, int right){
+        if(left > platesstack.size()-1 || right > platesstack.size()-1) return;
+        int valuefromright = (int) platesstack.get(right).remove(0); //remove the bottom value from the right stack.
+        platesstack.get(left).push(valuefromright); //push the above value back to left stack.
+        // check if more balancing can be done to fill the void in left stack.
+        balance(right, right+1); 
+    }
 }
